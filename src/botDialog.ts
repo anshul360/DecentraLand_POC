a20cd858cfe3a0c67aae13680c26406f4f4899f0
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2022-04-16 20:24:58 +0530

    bot dialog finalized

--- src/botDialog.ts
+++ src/botDialog.ts
@@ -1,7 +1,11 @@
 import { ButtonData, Dialog } from '@dcl/npc-scene-utils'
 import { alice } from './bot'
 import { products, visitedProducts, Product } from './listProducts'
-import { createScreen, prodVidPlay, prodVidPause } from './playProductVideo'
+import { createScreen, prodVidPlay, destroyScreen } from './playProductVideo'
+
+let orderId: string
+let sfBaseUrl: string
+let additionalDialog: Dialog[] = []
 
 export let AliceDialog: Dialog[] = [
   {
@@ -11,7 +15,11 @@ export let AliceDialog: Dialog[] = [
     text: 'Are you interested in knowing more about Salesforce products?',
     isQuestion: true,
     buttons: [
-      { label: 'Yes', goToDialog: 3 },
+      { label: 'Yes', goToDialog: 3, 
+        triggeredActions: () => {
+          additionalDialog.forEach((value, index, array) => {AliceDialog.push(value)})
+        }
+      },
       { label: 'No', goToDialog: 2 }
     ]
   },
@@ -25,17 +33,16 @@ export let AliceDialog: Dialog[] = [
   {
     text: 'We have wide range of business solutions to offer. Following are our core offerings. Click to see more details.',
     isQuestion: true,
-    buttons: buildButtons()
+    buttons: buildButtonsAndDialogs()
     
   }
 ]
 
-function buildButtons() {
+function buildButtonsAndDialogs() { //createScreen("");
     let buttons: ButtonData[] = []
-    
     products.forEach((value, index, arr) => {
         buttons.push({label: value.name, goToDialog: 4 + index})
-        AliceDialog.push({
+        additionalDialog.push({
           text: value.detail,
           isQuestion: true,
           buttons: [
@@ -48,37 +55,45 @@ function buildButtons() {
         })
     })
 
-    AliceDialog.push({
+    additionalDialog.push({
       text: "Do you wish to check other Products?",
       isQuestion: true,
       buttons: [
-        {label: "Yes", goToDialog: 3},
-        {label: "No", goToDialog: 4 + products.length + 1}
+        {label: "Yes", goToDialog: 3, triggeredActions: () => {destroyScreen()}},
+        {label: "No", goToDialog: 4 + products.length + 1, triggeredActions: () => {destroyScreen()}}
       ]
     })
 
-    AliceDialog.push({
+    additionalDialog.push({
       text: 'Okay, so do you want to buy the cool products you just checked out?',
       isQuestion: true,
       buttons: [
-        {label: "Yes", goToDialog: 3},
-        {label: "No", goToDialog: 4 + products.length + 1}
+        {label: "Yes", goToDialog: 4 + products.length + 3, triggeredActions: () => {/*call order create*/}},
+        {label: "No", goToDialog: 4 + products.length + 2}
       ]
     })
 
-    AliceDialog.push({
-      text: 'Okay, so do you want to buy any of the cool products you just checked out?',
+    additionalDialog.push({
+      text: 'Visit us again whenever you feel like. Have a great day!',
       isEndOfDialog: true,
       triggeredByNext: () => {
         alice.playAnimation('Goodbye', true, 2)
       }
     })
 
+    additionalDialog.push({
+      text: `You are Awesome! I've created the Order for you.`,
+      isQuestion: true,
+      buttons: [
+        {label: "Show Order", goToDialog: 4 + products.length + 2, triggeredActions: () => {openExternalURL(`${sfBaseUrl}/${orderId}`)}},
+        {label: "Thanks!", goToDialog: 4 + products.length + 2}
+      ]
+    })
     return buttons
 }
 
 function loadVideo(product: Product) {
   visitedProducts.push(product)
   createScreen(product.videoLink);
-
+  prodVidPlay()
 }
--- src/playProductVideo.ts
+++ src/playProductVideo.ts
@@ -1,7 +1,11 @@
-import { triggerEmote, PredefinedEmote } from "@decentraland/RestrictedActions"
+//import { triggerEmote, PredefinedEmote } from "@decentraland/RestrictedActions"
 
 let vidTexture: VideoTexture
+let screenUp: boolean = false
+let screen: Entity
 export function createScreen(videoUrl: string) {
+    destroyScreen()
+    
     let prodVidClip = new VideoClip(videoUrl)
     
     vidTexture = new VideoTexture(prodVidClip)
@@ -12,23 +16,24 @@ export function createScreen(videoUrl: string) {
     vidMaterial.specularIntensity = 0
     vidMaterial.metallic = 0
 
-    let screen = new Entity();
+    screenUp = true
+    screen = new Entity();
     screen.addComponent(new PlaneShape());
     screen.addComponent(
     new Transform({
-        position: new Vector3(67, 3.5, 40),
-        scale: new Vector3(10, 7, 100 ),
-        rotation: Quaternion.Euler(0, 245, 0),
+        position: new Vector3(64, 3, 56),
+        scale: new Vector3(7, 4.9, 100 ),
+        rotation: Quaternion.Euler(0, 285, 0),
     })
     );
     screen.addComponent(vidMaterial);
     screen.addComponent(
     new OnPointerDown(() => {
             vidTexture.playing = !vidTexture.playing;
-            if(!vidTexture.playing)
-                triggerEmote({ predefined: PredefinedEmote.HEAD_EXPLODDE })
-            else
-                triggerEmote({ predefined: PredefinedEmote.HANDS_AIR})
+            // if(!vidTexture.playing)
+            //     triggerEmote({ predefined: PredefinedEmote.HEAD_EXPLODDE })
+            // else
+            //     triggerEmote({ predefined: PredefinedEmote.HANDS_AIR})
         },
         { hoverText: "Watch Video" }
     )
@@ -44,3 +49,8 @@ export function prodVidPause() {
     vidTexture.pause()
 }
 
+export function destroyScreen() {
+    if(screenUp) {
+        engine.removeEntity(screen)
+    }
+}
