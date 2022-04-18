2669109fda43d872305f002cfdf9c850540bea43
Anshul Kumar <anshulkumar@salesforce.com>
2022-04-18 21:39:46 +0530

    grant type change

--- src/salesforceData.ts
+++ src/salesforceData.ts
@@ -6,11 +6,12 @@ export const consumerSecret: string = ""
 export const username: string = ""
 //password
 export const password: string = ""
+export const securitToken: string = ""
 
 export const proxyUrl: string = "https://agile-garden-07512.herokuapp.com/"
 export const sfBaseUrl: string = "https://login.salesforce.com"
 export const sfOauthEndpoint: string = `${sfBaseUrl}/services/oauth2/token?grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=`
-export const sfOauthUPEndpoint: string = `${sfBaseUrl}/services/oauth2/token?grant_type=password&username=${username}&password=${password}&client_id=${consumerKey}&client_secret=${consumerSecret}`
+export const sfOauthUPEndpoint: string = `${sfBaseUrl}/services/oauth2/token?grant_type=password&username=${username}&password=${password}${securitToken}&client_id=${consumerKey}&client_secret=${consumerSecret}`
 export const leadCreatePath: string = "/services/data/v54.0/sobjects/Lead/"
 export const leadConvertPath: string = "/services/apexrest/ConvertLead/"
 export const opptyLineItemCreateCompositePath: string = "/services/data/v54.0/composite/tree/OpportunityLineItem/"
